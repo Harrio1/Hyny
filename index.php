73a11b9f8c4a0b47d1d55bb35a2fdc6dd421704f
Harrio1 <rubenushka2006@gmail.com>
2024-04-04 10:01:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,19 +20,19 @@
         <div class="product">
             <img src="продукты/арбуз.jpg" alt="Продукт 1">
             <h2 class="cent">Арбузы</h2>
-            <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link"> +7 (905) 123-45-67</a></span></p>
+            <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link" href="login.php" > Показать номер</a></span></p>
             <p>Цена: 15.99 р.</p> 
         </div>
         <div class="product">
             <img src="продукты/бананы.jpg" alt="Продукт 2">
             <h2 class="cent">бананы </h2>
-            <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link">+7 (907) 155-69-16</a> </span></p>
+            <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link" href="login.php">Показать номер</a> </span></p>
             <p>Цена: 191.99 р.</p>
         </div>
         <div class="product">
             <img src="продукты/Дыня.jpeg" alt="Продукт 3">
             <h2 class="cent">Дыни</h2>
-            <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link">+7 (907) 155-69-16</a> </span></p>
+            <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link" href="login.php">Показать номер</a> </span></p>
             <p>Цена: 89.99 р.</p>
         </div>
             </div>
@@ -40,19 +40,19 @@
                 <div class="product">
                     <img src="продукты/морковь.jpg" alt="Продукт 1">
                     <h2 class="cent">Морковковки</h2>
-                    <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link"> +7 (905) 123-45-67</a></span></p>
+                    <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link" href="login.php"> Показать номер</a></span></p>
                     <p>Цена: 59.99 р.</p> 
                 </div>
                 <div class="product">
                     <img src="продукты/огурец.jpg" alt="Продукт 2">
                     <h2 class="cent">Огурецы </h2>
-                    <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link">+7 (907) 155-69-16</a> </span></p>
+                    <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link" href="login.php">Показать номер</a> </span></p>
                     <p>Цена: 80.99 р</p>
                 </div>
                 <div class="product">
                     <img src="продукты/помидоры.jpg" alt="Продукт 3">
                     <h2 class="cent">Помидоры</h2>
-                    <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link">+7 (907) 155-69-16</a> </span></p>
+                    <p>Для заказа товра обращайтесь по этому номеру: <span id="phoneNumber" class="phoneNumber"><a class="link" href="login.php">Показать номер</a> </span></p>
                     <p>Цена: 78.99 р.</p>
                 </div>
                 <div id="copyNotification" class="copy-notification"></div>
